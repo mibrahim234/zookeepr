8fc708a2df0a5ec2bb8db69694312adeeb3fb1c8
mibrahim234 <mohamm1417@gmail.com>
2021-10-13 19:14:37 -0400

    marense

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const { animals } = require('./data/animals');
 const express = require('express');
+const { animals } = require('./data/animals');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -42,12 +42,26 @@ function filterByQuery(query, animalsArray) {
   return filteredResults;
 }
 
+function findById(id, animalsArray) {
+  const result = animalsArray.filter(animal => animal.id === id)[0];
+  return result;
+}
+
 app.get('/api/animals', (req, res) => {
   let results = animals;
   if (req.query) {
     results = filterByQuery(req.query, results);
   }
   res.json(results);
+});
+
+app.get('/api/animals/:id', (req, res) => {
+  const result = findById(req.params.id, animals);
+  if (result) {
+    res.json(result);
+  } else {
+    res.send(404);
+  }
 });
 
 app.listen(PORT, () => {
