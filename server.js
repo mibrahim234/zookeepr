86f7133c13c93d47ce66b0b9845c847bed073981
mibrahim234 <mohamm1417@gmail.com>
2021-10-16 13:01:20 -0400

    fixed up html

--- server.js
+++ server.js
@@ -11,9 +11,9 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
-app.use(express.static('public'));
 
 
 
